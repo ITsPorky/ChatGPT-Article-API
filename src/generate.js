df339f97e40df5f99c3130a6824404316d028b54
ITsPorky <wc.walsh16@gmail.com>
2023-01-23 13:54:23 +1000

    Created function for fetching data from the OpenAI API

--- src/generate.js
+++ src/generate.js
@@ -1,6 +1,61 @@
-// OpenAI Calls
-const completion = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: generatePrompt(req.body.animal),
-  temperature: 0.6,
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  organization: "org-aUhn7ePhlxIjEKjGUXG1iIuF",
+  apiKey: "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",
 });
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+// OpenAI Calls
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured.",
+      },
+    });
+    return;
+  }
+
+  console.log(req.params.prompt);
+
+  const prompt = req.params.prompt || "";
+  if (prompt.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid animal",
+      },
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(prompt),
+      temperature: 0.29,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch (error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request.",
+        },
+      });
+    }
+  }
+}
+
+function generatePrompt(prompt) {
+  return `Write a kotaku styled article about ${prompt}:`;
+}
