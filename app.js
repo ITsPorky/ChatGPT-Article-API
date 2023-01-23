237b5a313051019dfba271ab7d9d6cb5675f3312
ITsPorky <wc.walsh16@gmail.com>
2023-01-23 13:53:29 +1000

    Added route for article generation

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+// import fetch from "node-fetch";
 import express from "express";
-
-const express = require("express");
+import generate from "./src/generate.js";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+// Routes
+app.get(`/`, (req, res) => {
   res.send("OpenAI ");
 });
 
+app.get(`/article/:prompt([^:$#@^&*]+)`, (req, res) => {
+  const article = generate(req, res);
+});
+
+// App Listen
 app.listen(port, () => {
   console.log(`ChatGPT-Article-API app listening on port ${port}`);
 });
