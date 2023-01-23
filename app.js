5e26c74001370d6c79dc8193c59512c0db9bc3c3
ITsPorky <wc.walsh16@gmail.com>
2023-01-23 16:34:17 +1000

    Updated Routes and cleaned file

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-// import fetch from "node-fetch";
+import schedule from "node-schedule";
 import express from "express";
 import generate from "./src/generate.js";
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Routes
+// Home Route
 app.get(`/`, (req, res) => {
-  res.send("OpenAI ");
+  res.send("OpenAI Article Generator");
+  res.sendFile(path.join(__dirname, "/README.html"));
 });
 
+// Route to generate article
 app.get(`/article/:prompt([^:$#@^&*]+)`, (req, res) => {
   const article = generate(req, res);
+  // Store article in firebase
+});
+
+// Schedule Event
+const job = schedule.scheduleJob("* * 6 * * *", function () {
+  // Collect a Idea/topic for the day
+  // Make call to OpenAI API with prompt
 });
 
 // App Listen
