35beb975c5c11ce40855696794f173e183269f69
ITsPorky <wc.walsh16@gmail.com>
2023-01-29 17:44:17 +1000

    Added route for articles and setup firebase connection

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ dotenv.config();
 import schedule from "node-schedule";
 import express from "express";
 import generate from "./src/generate.js";
+import { readArticleData } from "./src/firestore.js";
 const app = express();
 const port = process.env.PORT || 3000;
+
+// CORs Headers
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 // Home Route
 app.get(`/`, (req, res) => {
@@ -13,9 +24,16 @@ app.get(`/`, (req, res) => {
 });
 
 // Route to generate article
-app.get(`/article/:prompt([^:$#@^&*]+)`, (req, res) => {
+app.get(`/article/openai/:prompt([^:$#@^&*]+)`, (req, res) => {
   const article = generate(req, res);
   // Store article in firebase
+});
+
+// Retrieve article from Firebase
+app.get(`/article/:id([0-9]+)`, async (req, res) => {
+  const articleData = await readArticleData(req.params.id);
+  res.header("Content-Type", "application/json");
+  res.send(articleData);
 });
 
 // Schedule Event
